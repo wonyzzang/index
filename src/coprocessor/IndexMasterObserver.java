77ec5b06b75e9cbba90adb393be878c5e4fbbbc7
wonyzzang <jsw161@korea.ac.kr>
2016-09-07 02:17:56 +0900

    MasterObserver - create table with same column families

--- src/coprocessor/IndexMasterObserver.java
+++ src/coprocessor/IndexMasterObserver.java
@@ -1,8 +1,46 @@
 package coprocessor;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.coprocessor.BaseMasterObserver;
+import org.apache.hadoop.hbase.coprocessor.MasterCoprocessorEnvironment;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.master.MasterServices;
 
 public class IndexMasterObserver extends BaseMasterObserver {
+	
+	
+
+	@Override
+	public void preCreateTable(ObserverContext<MasterCoprocessorEnvironment> ctx, HTableDescriptor desc,
+			HRegionInfo[] regions) throws IOException {
+		
+		MasterServices master = ctx.getEnvironment().getMasterServices();
+		
+		Configuration conf = master.getConfiguration();
+
+		String tableName = desc.getNameAsString();
+		HColumnDescriptor[] columnDescs = desc.getColumnFamilies();
+
+		String[] columns = new String[columnDescs.length];
+		
+		HTableDescriptor indextable = new HTableDescriptor("testIndex");
+		for(HColumnDescriptor colDesc : columnDescs){
+			indextable.addFamily(colDesc);
+		}
+		
+		HBaseAdmin admin = new HBaseAdmin(conf);
+		admin.createTable(indextable);
+		
+		admin.close();
+	}
+	
+	
 	
 	
 }
