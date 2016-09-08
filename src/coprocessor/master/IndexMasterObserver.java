8589ea799944c7e9d3fd13470618a78887e17d8c
wonyzzang <jsw161@korea.ac.kr>
2016-09-08 10:38:48 +0900

    0908 end

--- src/coprocessor/master/IndexMasterObserver.java
+++ src/coprocessor/master/IndexMasterObserver.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.coprocessor.BaseMasterObserver;
 import org.apache.hadoop.hbase.coprocessor.MasterCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
@@ -23,17 +22,17 @@ public class IndexMasterObserver extends BaseMasterObserver {
 
 		Configuration conf = master.getConfiguration();
 
+		// consider only one column
 		String tableName = desc.getNameAsString();
-		HColumnDescriptor[] columnDescs = desc.getColumnFamilies();
 
-		HTableDescriptor indextable = new HTableDescriptor(tableName + "test");
-		for (HColumnDescriptor colDesc : columnDescs) {
-			indextable.addFamily(colDesc);
-		}
+		HTableDescriptor indextable = new HTableDescriptor(tableName + "_idx");
+		
+		HColumnDescriptor indCol = new HColumnDescriptor("ind");
+		indextable.addFamily(indCol);
+
 		HRegionInfo[] hRegionInfos = new HRegionInfo[] { new HRegionInfo(indextable.getName(), null, null) };
 
 		new CreateTableHandler(master, master.getMasterFileSystem(), master.getServerManager(), indextable, conf,
 				hRegionInfos, master.getCatalogTracker(), master.getAssignmentManager()).process();
-
 	}
 }
--- src/coprocessor/regionserver/IndexRegionObserver.java
+++ src/coprocessor/regionserver/IndexRegionObserver.java
@@ -1,7 +1,23 @@
 package coprocessor.regionserver;
 
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.RegionServerServices;
+import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
 
 public class IndexRegionObserver extends BaseRegionObserver {
+	
+	@Override
+	public void prePut(ObserverContext<RegionCoprocessorEnvironment> ctx, Put put, WALEdit edit, boolean writeToWAL)
+			throws IOException {
+		RegionServerServices regionServer = ctx.getEnvironment().getRegionServerServices();
+
+		String tableName = ctx.getEnvironment().getRegion().getRegionInfo().getTableNameAsString();
+		
+	}
 
 }
