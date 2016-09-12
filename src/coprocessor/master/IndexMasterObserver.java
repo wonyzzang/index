7aee6e5cdcef0171be66829d8233ba12e454b4ca
wonyzzang <jsw161@korea.ac.kr>
2016-09-12 11:28:03 +0900

    testing idx regionserver code

--- src/coprocessor/master/IndexMasterObserver.java
+++ src/coprocessor/master/IndexMasterObserver.java
@@ -2,37 +2,56 @@ package coprocessor.master;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableExistsException;
+import org.apache.hadoop.hbase.catalog.MetaReader;
 import org.apache.hadoop.hbase.coprocessor.BaseMasterObserver;
 import org.apache.hadoop.hbase.coprocessor.MasterCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.master.MasterServices;
 import org.apache.hadoop.hbase.master.handler.CreateTableHandler;
+import org.apache.hadoop.hbase.util.Bytes;
 
 public class IndexMasterObserver extends BaseMasterObserver {
+	
+	private static final Log LOG = LogFactory.getLog(IndexMasterObserver.class.getName());
 
 	@Override
 	public void preCreateTable(ObserverContext<MasterCoprocessorEnvironment> ctx, HTableDescriptor desc,
 			HRegionInfo[] regions) throws IOException {
-
+		LOG.info("PreCreateTable START");
+	
 		MasterServices master = ctx.getEnvironment().getMasterServices();
 
 		Configuration conf = master.getConfiguration();
 
 		// consider only one column
 		String tableName = desc.getNameAsString();
-
-		HTableDescriptor indextable = new HTableDescriptor(tableName + "_idx");
+		String idxTableName = tableName + "_idx";
+		
+		boolean isTableExist = MetaReader.tableExists(master.getCatalogTracker(), tableName);
+		boolean isIdxTableExist = MetaReader.tableExists(master.getCatalogTracker(), idxTableName);
+		
+		if(!(isTableExist&&isIdxTableExist)){
+			LOG.error("Table already exists");
+			throw new TableExistsException("Table " + tableName + " already exist.");
+		}
+		
+		HTableDescriptor indextable = new HTableDescriptor(idxTableName);
 		
 		HColumnDescriptor indCol = new HColumnDescriptor("ind");
 		indextable.addFamily(indCol);
 
-		HRegionInfo[] hRegionInfos = new HRegionInfo[] { new HRegionInfo(indextable.getName(), null, null) };
+		HRegionInfo[] hRegionInfos = new HRegionInfo[] { new HRegionInfo(Bytes.toBytes(idxTableName), null, null) };
 
 		new CreateTableHandler(master, master.getMasterFileSystem(), master.getServerManager(), indextable, conf,
 				hRegionInfos, master.getCatalogTracker(), master.getAssignmentManager()).process();
+		
+		LOG.info("PreCreateTable END");
 	}
 }
--- src/coprocessor/regionserver/IndexRegionObserver.java
+++ src/coprocessor/regionserver/IndexRegionObserver.java
@@ -1,23 +1,56 @@
 package coprocessor.regionserver;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.RegionServerServices;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import coprocessor.master.IndexMasterObserver;
 
 public class IndexRegionObserver extends BaseRegionObserver {
+	
+	private static final Log LOG = LogFactory.getLog(IndexRegionObserver.class.getName());
 	
 	@Override
 	public void prePut(ObserverContext<RegionCoprocessorEnvironment> ctx, Put put, WALEdit edit, boolean writeToWAL)
 			throws IOException {
+		LOG.info("PrePut START");
+		
 		RegionServerServices regionServer = ctx.getEnvironment().getRegionServerServices();
 
 		String tableName = ctx.getEnvironment().getRegion().getRegionInfo().getTableNameAsString();
 		
+		HTableDescriptor tableDesc = new HTableDescriptor(tableName);
+		HColumnDescriptor columnDescs[]=tableDesc.getColumnFamilies();
+		
+		List<KeyValue> list = put.get(columnDescs[0].getName(), Bytes.toBytes("qual1"));
+		KeyValue num0 = list.get(0);
+		byte value[] = num0.getValue();
+		String rowkey = columnDescs[0].getNameAsString()+"qual1"+Bytes.toString(value);
+		
+		Put p = new Put(Bytes.toBytes(rowkey));
+		p.add(Bytes.toBytes("ind"), Bytes.toBytes("ind"), Bytes.toBytes("col1"));
+		
+		HTableDescriptor idxTableDesc = new HTableDescriptor(tableName+"_idx");
+		
+		List<HRegion> regions = ctx.getEnvironment().getRegionServerServices().getOnlineRegions(idxTableDesc.getName());
+		HRegion region = regions.get(0);
+		region.put(p);
+		
+		LOG.info("PrePut END");
 	}
 
 }
